3a272e4a0cff42ef651d1c98d1f8c63eaa98a7d8
Lucy Wall <lhwall@gmail.com>
2019-03-05 16:11:16 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,7 @@ class ProductsController < ApplicationController
   def product_data
     product = Product.find(params[:id])
     render json: product.to_json(only: [:id, :name, :price, :description, :inventory])
+  end 
 
   private
 
